04e826a353e5aa8467aca9f5326512ec2a034794
azimut <azimut.github@protonmail.com>
2020-03-04 22:05:54 -0300

    uncomment

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ snitch
 
 An OTP application
 
+%% inet_res:nslookup("tesla.com",in,txt,[{{8,8,8,8}, 53}]).
+%% inet_res:lookup("www.starbucks.com",in,cname,[{nameservers, [{{8,8,8,8}, 53}]}]).
+
 Build
 -----
 
--- apps/snitch/src/snitch_resolver.erl
+++ apps/snitch/src/snitch_resolver.erl
@@ -1,16 +1,9 @@
 -module(snitch_resolver).
 
 -include_lib("kernel/src/inet_dns.hrl").
-%% 208.67.222.222
-%% 208.67.220.220
-%% 1.1.1.1
-%% 9.9.9.9
 -define(DNS_SERVERS, [{8,8,8,8},{8,8,4,4},{1,1,1,1},{9,9,9,9}]).
 
 -export([do_query/2]).
-
-%% inet_res:nslookup("tesla.com",in,txt,[{{8,8,8,8}, 53}]).
-%% inet_res:lookup("www.starbucks.com",in,cname,[{nameservers, [{{8,8,8,8}, 53}]}]).
 
 get_dns_server() ->
     [{lists:nth(rand:uniform(erlang:length(?DNS_SERVERS)),?DNS_SERVERS), 53}].
