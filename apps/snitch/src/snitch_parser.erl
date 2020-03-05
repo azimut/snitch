4d00deb7055e65f01d4b71c5b011940c915eb368
azimut <azimut.github@protonmail.com>
2020-03-05 05:27:43 -0300

    sort things out

--- apps/snitch/src/snitch_parser.erl
+++ apps/snitch/src/snitch_parser.erl
@@ -2,37 +2,43 @@
 -include_lib("kernel/src/inet_dns.hrl").
 -export([process_domain/1]).
 
+%% API
+
+process_domain(Domain)    ->
+    CName = snitch_resolver:do_query(Domain, cname),
+    process_domain(Domain, CName).
+
+%% Internal functions
+
+process_domain(Domain, []) ->
+    query_and_store(Domain, mx),
+    query_and_store(Domain, txt),
+    query_and_store(Domain, aaaa),
+    query_and_store(Domain, a),
+    query_and_store(Domain, ns);
+process_domain(Domain, _)  ->
+    query_and_store(Domain, cname).
+
+format_data(#dns_rr{type = a}=X)     -> inet:ntoa(X#dns_rr.data);
+format_data(#dns_rr{type = aaaa}=X)  -> inet:ntoa(X#dns_rr.data);
+format_data(#dns_rr{type = mx}=X)    -> erlang:element(2, X#dns_rr.data);
+format_data(#dns_rr{type = ns}=X)    -> X#dns_rr.data;
+format_data(#dns_rr{type = cname}=X) -> X#dns_rr.data;
+format_data(#dns_rr{type = txt}=X)   -> lists:nth(1,X#dns_rr.data).
+
 answer_data([]) ->
     [];
 answer_data([#dns_rr{}=X|Xs]) ->
     Data = [format_data(X)] ++ answer_data(Xs),
     lists:sort(Data).
 
-format_data(#dns_rr{type = mx}=X)    -> erlang:element(2, X#dns_rr.data);
-format_data(#dns_rr{type = a}=X)     -> inet:ntoa(X#dns_rr.data);
-format_data(#dns_rr{type = aaaa}=X)  -> inet:ntoa(X#dns_rr.data);
-format_data(#dns_rr{type = ns}=X)    -> X#dns_rr.data;
-format_data(#dns_rr{type = cname}=X) -> X#dns_rr.data;
-format_data(#dns_rr{type = txt}=X)   -> lists:nth(0, X#dns_rr.data).
-
-store_record(_Domain, _Type, _LRecord) ->
-    true or false.
-
 query_and_store(Domain, cname) ->
     IsCName = fun (R) -> R#dns_rr.type == cname end,
     A = snitch_resolver:do_query(Domain, a),
     CName = lists:filter(IsCName, A),
     Data = answer_data(CName),
-    store_record(Domain, cname, Data);
-query_and_store(Domain, Type) ->
-    Records = snitch_resolver:do_query(Domain, Type),
+    snitch_store:store(Domain, cname, Data);
+query_and_store(Domain, Type)  ->
+    Records = snitch_resolver:do_pure(Domain, Type),
     Data = answer_data(Records),
-    store_record(Domain, Type, Data).
-
-process_domain(Domain) ->
-    CName = snitch_resolver:do_query(Domain, cname),
-    proces_domain(Domain, CName).
-process_domain(Domain, [])  ->
-    snitch_resolver:do_query(Domain, mx);
-process_domain(Domain, [_]) ->
-
+    snitch_store:store(Domain, Type, Data).
