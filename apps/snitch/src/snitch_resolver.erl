8a20ba8bd167054cddfba5780e71b35b89e89f26
azimut <azimut.github@protonmail.com>
2020-03-05 05:27:16 -0300

    add pure resolver

--- apps/snitch/src/snitch_resolver.erl
+++ apps/snitch/src/snitch_resolver.erl
@@ -3,7 +3,7 @@
 -include_lib("kernel/src/inet_dns.hrl").
 -define(DNS_SERVERS, [{8,8,8,8},{8,8,4,4},{1,1,1,1},{9,9,9,9}]).
 
--export([do_query/2]).
+-export([do_query/2,do_pure/2]).
 
 get_dns_server() ->
     [{lists:nth(rand:uniform(erlang:length(?DNS_SERVERS)),?DNS_SERVERS), 53}].
@@ -13,3 +13,7 @@ do_query(Domain, Type) ->
     TimeOut = 5 * 1000,
     {ok, R} = inet_res:nnslookup(Domain, in, Type, NSs, TimeOut),
     R#dns_rec.anlist.
+
+do_pure(Domain, Type) ->
+    Answer = do_query(Domain, Type),
+    lists:filter(fun (R) -> R#dns_rr.type == Type end, Answer).
