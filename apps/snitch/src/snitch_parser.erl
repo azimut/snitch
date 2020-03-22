9b16688f14535c3b06d6ec8443899c6edb43be38
azimut <azimut.github@protonmail.com>
2020-03-22 18:33:45 -0300

    format/reorder

--- apps/snitch/src/snitch_parser.erl
+++ apps/snitch/src/snitch_parser.erl
@@ -4,21 +4,13 @@
 
 %% API
 
-process_domain(Domain)    ->
+process_domain(Domain) ->
     CName = snitch_resolver:do_query(Domain, cname),
     process_domain(Domain, CName).
 
 %% Internal functions
 
-process_domain(Domain, []) ->
-    query_and_store(Domain, mx),
-    query_and_store(Domain, txt),
-    query_and_store(Domain, aaaa),
-    query_and_store(Domain, a),
-    query_and_store(Domain, ns);
-process_domain(Domain, _)  ->
-    query_and_store(Domain, cname).
-
+%% TODO: soa
 format_data(#dns_rr{type = a}=X)     -> inet:ntoa(X#dns_rr.data);
 format_data(#dns_rr{type = aaaa}=X)  -> inet:ntoa(X#dns_rr.data);
 format_data(#dns_rr{type = mx}=X)    -> erlang:element(2, X#dns_rr.data);
@@ -26,8 +18,7 @@ format_data(#dns_rr{type = ns}=X)    -> X#dns_rr.data;
 format_data(#dns_rr{type = cname}=X) -> X#dns_rr.data;
 format_data(#dns_rr{type = txt}=X)   -> lists:nth(1,X#dns_rr.data).
 
-answer_data([]) ->
-    [];
+answer_data([])               -> [];
 answer_data([#dns_rr{}=X|Xs]) ->
     Data = [format_data(X)] ++ answer_data(Xs),
     lists:sort(Data).
@@ -42,3 +33,12 @@ query_and_store(Domain, Type)  ->
     Records = snitch_resolver:do_pure(Domain, Type),
     Data = answer_data(Records),
     snitch_store:store(Domain, Type, Data).
+
+process_domain(Domain, []) ->
+    query_and_store(Domain, mx),
+    query_and_store(Domain, txt),
+    query_and_store(Domain, aaaa),
+    query_and_store(Domain, a),
+    query_and_store(Domain, ns);
+process_domain(Domain, _)  ->
+    query_and_store(Domain, cname).
