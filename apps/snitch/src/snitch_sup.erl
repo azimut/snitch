fef09a035b16d338e87f2cb290e75604cbbfa99f
azimut <azimut.github@protonmail.com>
2020-03-26 22:39:08 -0300

    make supervisor restart

--- apps/snitch/src/snitch_sup.erl
+++ apps/snitch/src/snitch_sup.erl
@@ -27,7 +27,7 @@ start_link() ->
 %%                  modules => modules()}   % optional
 init([]) ->
     SupFlags = #{strategy  => one_for_all,
-                 intensity => 0,
+                 intensity => 2,
                  period    => 1},
     ChildSpecs = #{id       => calendario,
                    start    => {snitch_scheduler, start_link, []},
