1213a0fc57ae9e80e4b78221a20cd330312a93c0
azimut <azimut.github@protonmail.com>
2020-03-26 22:37:19 -0300

    add hrl

---
+++ apps/snitch/src/db.hrl
@@ -0,0 +1,6 @@
+-define(DNS_SERVERS, [{8,8,8,8},{8,8,4,4},{1,1,1,1},{9,9,9,9}]).
+-define(DNS_TIMEOUT, 5).
+-define(SERVER, ?MODULE).
+-define(TABLE_NAME, mytable).
+-define(DEFAULT_TICK, 60).
+-define(INIT_TICK, 5).
--- apps/snitch/src/snitch_resolver.erl
+++ apps/snitch/src/snitch_resolver.erl
@@ -1,7 +1,7 @@
 -module(snitch_resolver).
 
 -include_lib("kernel/src/inet_dns.hrl").
--define(DNS_SERVERS, [{8,8,8,8},{8,8,4,4},{1,1,1,1},{9,9,9,9}]).
+-include("db.hrl").
 
 -export(
    [
@@ -10,11 +10,14 @@
    ]).
 
 get_dns_server() ->
-    [{lists:nth(rand:uniform(erlang:length(?DNS_SERVERS)),?DNS_SERVERS), 53}].
+    [{lists:nth(
+        rand:uniform(erlang:length(?DNS_SERVERS)),
+        ?DNS_SERVERS),
+      53}].
 
 do_query(Domain, Type) ->
     NSs = get_dns_server(),
-    TimeOut = 5 * 1000,
+    TimeOut = ?DNS_TIMEOUT * 1000,
     {ok, R} = inet_res:nnslookup(Domain, in, Type, NSs, TimeOut),
     R#dns_rec.anlist.
 
