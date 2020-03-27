58213b8b0441bfd6eea72229daa5a541f9c750e9
azimut <azimut.github@protonmail.com>
2020-03-26 23:02:25 -0300

    dets type

--- apps/snitch/src/snitch_disk.erl
+++ apps/snitch/src/snitch_disk.erl
@@ -1,17 +1,21 @@
 -module(snitch_disk).
--export([save/0,load/0]).
 -include("db.hrl").
+-export(
+   [
+    save/0,
+    load/0
+   ]).
 
 %% https://github.com/bernardd/Conway
 %% /blob/d1af91a0b535978995c9644a9a91a2e658e478a6/cell_store.erl
 load() ->
-    {ok, Dets} = dets:open_file(?TABLE_NAME, []),
+    {ok, Dets} = dets:open_file(?TABLE_NAME, [{type,bag}]),
     ets:delete_all_objects(?TABLE_NAME),
     ets:from_dets(?TABLE_NAME, Dets),
     dets:close(Dets).
 
 save() -> 
-    {ok, Dets} = dets:open_file(?TABLE_NAME, []),
+    {ok, Dets} = dets:open_file(?TABLE_NAME, [{type,bag}]),
     ets:to_dets(?TABLE_NAME, Dets),
     dets:close(Dets).
 
