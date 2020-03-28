0ea00351eddde874100098828559ebb28b5a51be
azimut <azimut.github@protonmail.com>
2020-03-28 01:54:16 -0300

    add date into ets, store_on_difference

--- apps/snitch/src/snitch_store.erl
+++ apps/snitch/src/snitch_store.erl
@@ -10,13 +10,14 @@
 
 %% API
 
-store(Domain, Type, List) ->
-    ets:insert(?TABLE_NAME, {Domain, Type, List}).
+store(Domain, Type, Data) ->
+    Time = calendar:local_time(),
+    ets:insert(?TABLE_NAME, {Domain, Type, Data, Time}).
 
 store_and_alert(Domain, Type, ListNew) ->
     ListOld = lookup(Domain, Type),
     alert_on_difference(Domain, Type, ListNew, ListOld),
-    store(Domain, Type, ListNew).
+    store_on_difference(Domain, Type, ListNew, ListOld).
 
 %% Private Functions
 
@@ -30,30 +31,50 @@ slab([F|R]) ->
         false -> slab(F) ++ slab(R)
     end.
 
+format_string(String) ->
+    io:format("~s~n",[String]).
+
 format_list(Format, List) -> 
     lists:foreach(fun (S) -> io:format(Format, [S]) end, List).
 
 lookup(Domain, Type) ->
     lists:sort(
       slab(
-        ets:match(?TABLE_NAME, {Domain, Type, '$1'}))).
+        ets:match(?TABLE_NAME, {Domain, Type, '$1', '_'}))).
 
-alert(Domain, Type, New, _)
+notify(Title, Msg) ->
+    S = io_lib:format("/usr/bin/notify-send --urgency critical '~s' '~s'", [Title, Msg]),
+    os:cmd(S).
+
+alert(Domain, Type, New=[H|_],_)
   when erlang:length(New) =:= 1 ->
-    io:format("ALERT! domain (~s) has new thing for type (~s)~n", [Domain,Type]),
-    format_list("NEW ~s~n", New);
+    io:format("NEW! data for domain ~s~n", [Domain]),
+    Msg = io_lib:format("type ~s has new data:~n~s",[Type, H]),
+    format_string(Msg),
+    notify(Domain, Msg);
 alert(Domain, Type, New, Old) ->
-    io:format("ALERT! domain (~s) changed for type (~s)~n", [Domain,Type]),
+    io:format("CHANGE! data for domain ~s~n", [Domain]),
+    Msg = io_lib:format("type (~s) changed", [Type]),
+    format_string(Msg),
+    notify(Domain, Msg),
     format_list("OLD ~s~n", Old),
     format_list("NEW ~s~n", New).
 
-alert_on_difference(_, _, _, [])                    -> false;
-alert_on_difference(_, _, [], _)                    -> false;
-alert_on_difference(Domain, Type, ListNew, ListOld) ->
-    SetNew = sets:from_list(ListNew),
-    SetOld = sets:from_list(ListOld),
-    case sets:is_subset(SetNew, SetOld) of
+is_subset(ListA, ListB) ->
+    SetA = sets:from_list(ListA),
+    SetB = sets:from_list(ListB),
+    sets:is_subset(SetA, SetB).
+
+alert_on_difference(_,_,_Dns=[_|_],_Ets=[]) -> ok; % First time
+alert_on_difference(Domain, Type, Dns, Ets) ->
+    case is_subset(Dns, Ets) of
         true  -> ok;
-        false -> alert(Domain, Type, ListNew, ListOld)
+        false -> alert(Domain, Type, Dns, Ets)
     end.
 
+store_on_difference(_,_,_Dns=[],_)           -> ok;
+store_on_difference(Domain, Type, Dns, Ets) ->
+    case is_subset(Dns, Ets) of
+        true  -> ok;
+        false -> store(Domain, Type, Dns)
+    end.
