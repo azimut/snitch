b6f2c7c342c923c54dff05ece04905f40ff2d73d
azimut <azimut.github@protonmail.com>
2020-03-31 01:16:31 -0300

    rename funcs

--- apps/snitch/src/snitch_parser.erl
+++ apps/snitch/src/snitch_parser.erl
@@ -12,27 +12,26 @@ query_all(Domain) ->
 %% Internal functions
 
 %% TODO: soa
-format_data(#dns_rr{type = a}=X)     -> inet:ntoa(X#dns_rr.data);
-format_data(#dns_rr{type = aaaa}=X)  -> inet:ntoa(X#dns_rr.data);
-format_data(#dns_rr{type = mx}=X)    -> erlang:element(2, X#dns_rr.data);
-format_data(#dns_rr{type = ns}=X)    -> X#dns_rr.data;
-format_data(#dns_rr{type = cname}=X) -> X#dns_rr.data;
-format_data(#dns_rr{type = txt}=X)   -> lists:nth(1,X#dns_rr.data).
+get_data(#dns_rr{type = a}=X)     -> inet:ntoa(X#dns_rr.data);
+get_data(#dns_rr{type = aaaa}=X)  -> inet:ntoa(X#dns_rr.data);
+get_data(#dns_rr{type = mx}=X)    -> erlang:element(2, X#dns_rr.data);
+get_data(#dns_rr{type = ns}=X)    -> X#dns_rr.data;
+get_data(#dns_rr{type = cname}=X) -> X#dns_rr.data;
+get_data(#dns_rr{type = txt}=X)   -> lists:nth(1,X#dns_rr.data).
 
-answer_data(ok,#dns_rec{}=Record) -> answer_data(ok, Record#dns_rec.anlist);
-answer_data(error,Error)          -> [Error];
-answer_data(ok,[])                -> [];
-answer_data(ok,[#dns_rr{}=X|Xs])  ->
-    Data = [format_data(X)] ++ answer_data(ok,Xs),
-    lists:sort(Data).
+get_answers([])               -> [];
+get_answers([#dns_rr{}=X|Xs]) -> [get_data(X)] ++ get_answers(Xs).
+
+answers(ok, #dns_rec{}=Record) -> lists:sort(get_answers(Record#dns_rec.anlist));
+answers(error, Error)          -> [Error].
 
 query_and_store(Domain, cname) -> % get full chain up to A
     {Status, Record} = snitch_resolver:do_query(Domain, a),
-    Data = answer_data(Status,Record),
+    Data = answers(Status,Record),
     snitch_store:store_and_alert(Domain, cname, Data);
 query_and_store(Domain, Type)  -> % get only record requested
     {Status, Record} = snitch_resolver:do_pure(Domain, Type),
-    Data = answer_data(Status, Record),
+    Data = answers(Status, Record),
     snitch_store:store_and_alert(Domain, Type, Data).
 
 query_all(Domain, ok, #dns_rec{anlist=[]}) ->
