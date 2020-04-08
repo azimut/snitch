2e53ccfd22e04521578a7b3ca30a05e8bd6ebb99
azimut <azimut.github@protonmail.com>
2020-04-08 09:32:46 -0300

    typo

--- apps/snitch/src/snitch_locus.erl
+++ apps/snitch/src/snitch_locus.erl
@@ -12,7 +12,7 @@ asns(Ips) ->
     [ asn(X) || X <- Ips ].
 
 new_asns(DnsIps, EtsIps) ->
-    DnsAsns = sets:from_lists(asns(DnsIps)),
-    EtsAsns = sets:from_lists(asns(EtsIps)),
+    DnsAsns = sets:from_list(asns(DnsIps)),
+    EtsAsns = sets:from_list(asns(EtsIps)),
     sets:to_list(
       sets:subtract(DnsAsns,EtsAsns)).
