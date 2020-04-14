795519634eb796e9b1df6c72836f1e950eeeeb80
azimut <azimut.github@protonmail.com>
2020-04-14 19:19:38 -0300

    Minor

--- apps/snitch/src/snitch_client.erl
+++ apps/snitch/src/snitch_client.erl
@@ -3,10 +3,10 @@
 -export([delete/1,reload/0,checkpoint/0,add/1]).
 -export([get/1,all/0]).
 
+add(Domain)    -> gen_server:cast(snitch_scheduler, {add, Domain}).
 delete(Domain) -> gen_server:cast(snitch_scheduler, {delete, Domain}).
 reload()       -> gen_server:cast(snitch_scheduler, reload).
 checkpoint()   -> gen_server:cast(snitch_scheduler, checkpoint).
-add(Domain)    -> gen_server:cast(snitch_scheduler, {add, Domain}).
 all()          -> gen_server:call(snitch_scheduler, all).
 get(Domain)    ->
     Ret = gen_server:call(snitch_scheduler, {get, Domain}),
