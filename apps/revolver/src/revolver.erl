26c7bec369e93ccedb15d790144dca0465ecccd9
azimut <azimut.github@protonmail.com>
2020-12-29 18:24:33 -0300

    revolver: lookup/4, simpler answers(), rename get_data

--- apps/revolver/src/revolver.erl
+++ apps/revolver/src/revolver.erl
@@ -1,14 +1,17 @@
 -module(revolver).
 -include_lib("kernel/src/inet_dns.hrl").
 
--export([lookup/2,lookup/3]).
+-export([lookup/2,lookup/3,lookup/4]).
 
 lookup(Domain, Type) ->
     lookup(Domain, Type, rand_dns_server()).
 lookup(Domain, Type, NSs) ->
     Timeout = dns_timeout() * 1000,
+    lookup(Domain, Type, NSs, Timeout).
+lookup(Domain, Type, NSs, Timeout) ->
     {Status, Record} = inet_res:nnslookup(Domain, in, Type, NSs, Timeout),
-    answers(Status, Record).
+    Reply = answers(Record),
+    {Status, Reply}.
 
 %% Internal Functions
 
@@ -33,18 +36,16 @@ dns_timeout() ->
         _ -> 10
     end.
 
-answers(error, Error)           ->
-    {error, Error};
-answers(ok, #dns_rec{anlist=A}) ->
-    {ok, get_data(A)}.
+answers(#dns_rec{anlist=Answers})  -> parse(Answers);
+answers(Error) when is_atom(Error) -> Error.
 
-get_data([])                      -> [];
-get_data([#dns_rr{}=X|Xs])        -> [get_data(X)] ++ get_data(Xs);
-get_data(#dns_rr{type = a}=X)     -> inet:ntoa(X#dns_rr.data);
-get_data(#dns_rr{type = aaaa}=X)  -> inet:ntoa(X#dns_rr.data);
-get_data(#dns_rr{type = mx}=X)    -> erlang:element(2, X#dns_rr.data);
-get_data(#dns_rr{type = soa}=X)   -> erlang:integer_to_list(
-                                       erlang:element(3, X#dns_rr.data));
-get_data(#dns_rr{type = ns}=X)    -> X#dns_rr.data;
-get_data(#dns_rr{type = cname}=X) -> X#dns_rr.data;
-get_data(#dns_rr{type = txt}=X)   -> lists:nth(1,X#dns_rr.data).
+parse([])                      -> [];
+parse([#dns_rr{}=X|Xs])        -> [parse(X)] ++ parse(Xs);
+parse(#dns_rr{type = a}=X)     -> inet:ntoa(X#dns_rr.data);
+parse(#dns_rr{type = aaaa}=X)  -> inet:ntoa(X#dns_rr.data);
+parse(#dns_rr{type = mx}=X)    -> erlang:element(2, X#dns_rr.data);
+parse(#dns_rr{type = soa}=X)   -> erlang:integer_to_list(
+                                    erlang:element(3, X#dns_rr.data));
+parse(#dns_rr{type = ns}=X)    -> X#dns_rr.data;
+parse(#dns_rr{type = cname}=X) -> X#dns_rr.data;
+parse(#dns_rr{type = txt}=X)   -> lists:nth(1,X#dns_rr.data).
