93734a7bc5be8d8cc1978384badcea341f8359c4
azimut <azimut.github@protonmail.com>
2021-11-20 15:14:22 -0300

    sheriff_pony: rename reply for lokup, remove defaults

--- apps/sheriff/src/sheriff_pony.erl
+++ apps/sheriff/src/sheriff_pony.erl
@@ -1,26 +1,24 @@
 -module(sheriff_pony).
--export([express/2,express/3]).
+-export([express/2]).
 
-express(From, Domain)       ->
+express(From, Domain) ->
     {State, Data} = revolver:lookup(Domain, cname),
     gishgallop(State, From, Domain, Data).
-express(From, Domain, Type) ->
-    From ! reply(Domain, Type).
 
 %% Internal Functions
 
-gishgallop(ok,From,Domain,[])       ->
-    From ! reply(Domain,a),
-    From ! reply(Domain,aaaa),
-    From ! reply(Domain,mx),
-    From ! reply(Domain,soa),
-    From ! reply(Domain,ns),
-    From ! reply(Domain,txt);
+gishgallop(ok,From,Domain,[]) ->
+    From ! lookup(Domain,a),
+    From ! lookup(Domain,aaaa),
+    From ! lookup(Domain,mx),
+    From ! lookup(Domain,soa),
+    From ! lookup(Domain,ns),
+    From ! lookup(Domain,txt);
 gishgallop(Status,From,Domain,Data) ->
-    From ! reply(Domain, cname, Status, Data).
+    From ! lookup(Domain, cname, Status, Data).
 
-reply(Domain, Type) ->
+lookup(Domain, Type) ->
     {Status, Data} = revolver:lookup(Domain, Type),
-    reply(Domain, Type, Status, Data).
-reply(Domain, Type, Status, Data) ->
+    lookup(Domain, Type, Status, Data).
+lookup(Domain, Type, Status, Data) ->
     {Status, Data, Domain, Type}.
