3029462be15e4da6ab7953ee8916fbf485207c1f
azimut <azimut.github@protonmail.com>
2021-11-20 15:14:02 -0300

    sheriff_holster: remove sync and async with type

--- apps/sheriff/src/sheriff_holster.erl
+++ apps/sheriff/src/sheriff_holster.erl
@@ -1,18 +1,13 @@
 -module(sheriff_holster).
 -behaviour(gen_server).
--export([async_lookup/2,async_lookup/3,sync_lookup/2]).
+-export([async_lookup/2]).
 -export([start_link/0,init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3, format_status/2]).
 -define(SERVER, ?MODULE).
 -record(state, {}).
 
-sync_lookup(Domain, Type) ->
-    gen_server:call(?MODULE, {lookup, Domain, Type}).
-
 async_lookup(From, Domain)       ->
     gen_server:cast(?SERVER, {lookup, From, Domain}).
-async_lookup(From, Domain, Type) ->
-    gen_server:cast(?SERVER, {lookup, From, Domain, Type}).
 
 %% Default Functions
 
@@ -23,22 +18,14 @@ init([]) ->
     process_flag(trap_exit, true),
     {ok, #state{}}.
 
-handle_call({lookup, Domain, Type}, _From, State) ->
-    Reply = revolver:lookup(Domain, Type),
-    {reply, Reply, State};
-handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
-
 handle_cast({lookup, From, Domain}, State)       ->
     erlang:spawn(sheriff_pony, express, [From, Domain]),
-    {noreply, State};
-handle_cast({lookup, From, Domain, Type}, State) ->
-    erlang:spawn(sheriff_pony, express, [From, Domain, Type]),
     {noreply, State};
 handle_cast(_Request, State)                     ->
     {noreply, State}.
 
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
 handle_info(_Info, State) ->
     {noreply, State}.
 terminate(_Reason, _State) ->
