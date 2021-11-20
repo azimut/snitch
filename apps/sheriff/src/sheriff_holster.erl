4d6cfad0e51d7abad7c87317a944cfc454f41a87
azimut <azimut.github@protonmail.com>
2021-11-20 15:01:52 -0300

    sheriff_holster: remove cruft and trust our insticts (?)

--- apps/sheriff/src/sheriff_holster.erl
+++ apps/sheriff/src/sheriff_holster.erl
@@ -1,71 +1,28 @@
-%%%-------------------------------------------------------------------
-%%% @author azimut <azimut.github@protonmail.com>
-%%% @copyright (C) 2020, azimut
-%%% @doc
-%%%
-%%% @end
-%%% Created : 20 Dec 2020 by azimut <azimut.github@protonmail.com>
-%%%-------------------------------------------------------------------
 -module(sheriff_holster).
 -behaviour(gen_server).
--export([start_link/0]).
 -export([async_lookup/2,async_lookup/3,sync_lookup/2]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+-export([start_link/0,init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3, format_status/2]).
 -define(SERVER, ?MODULE).
 -record(state, {}).
 
-%%%===================================================================
-%%% API
-%%%===================================================================
+sync_lookup(Domain, Type) ->
+    gen_server:call(?MODULE, {lookup, Domain, Type}).
 
+async_lookup(From, Domain)       ->
+    gen_server:cast(?SERVER, {lookup, From, Domain}).
+async_lookup(From, Domain, Type) ->
+    gen_server:cast(?SERVER, {lookup, From, Domain, Type}).
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%% @end
-%%--------------------------------------------------------------------
--spec start_link() -> {ok, Pid :: pid()} |
-          {error, Error :: {already_started, pid()}} |
-          {error, Error :: term()} |
-          ignore.
+%% Default Functions
+
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
-%%%===================================================================
-%%% gen_server callbacks
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%% @end
-%%--------------------------------------------------------------------
--spec init(Args :: term()) -> {ok, State :: term()} |
-          {ok, State :: term(), Timeout :: timeout()} |
-          {ok, State :: term(), hibernate} |
-          {stop, Reason :: term()} |
-          ignore.
 init([]) ->
     process_flag(trap_exit, true),
     {ok, #state{}}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%% @end
-%%--------------------------------------------------------------------
--spec handle_call(Request :: term(), From :: {pid(), term()}, State :: term()) ->
-          {reply, Reply :: term(), NewState :: term()} |
-          {reply, Reply :: term(), NewState :: term(), Timeout :: timeout()} |
-          {reply, Reply :: term(), NewState :: term(), hibernate} |
-          {noreply, NewState :: term()} |
-          {noreply, NewState :: term(), Timeout :: timeout()} |
-          {noreply, NewState :: term(), hibernate} |
-          {stop, Reason :: term(), Reply :: term(), NewState :: term()} |
-          {stop, Reason :: term(), NewState :: term()}.
 handle_call({lookup, Domain, Type}, _From, State) ->
     Reply = revolver:lookup(Domain, Type),
     {reply, Reply, State};
@@ -73,20 +30,6 @@ handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
 
-sync_lookup(Domain, Type) ->
-    gen_server:call(?MODULE, {lookup, Domain, Type}).
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%% @end
-%%--------------------------------------------------------------------
--spec handle_cast(Request :: term(), State :: term()) ->
-          {noreply, NewState :: term()} |
-          {noreply, NewState :: term(), Timeout :: timeout()} |
-                                  {noreply, NewState :: term(), hibernate} |
-                                  {stop, Reason :: term(), NewState :: term()}.
 handle_cast({lookup, From, Domain}, State)       ->
     erlang:spawn(sheriff_pony, express, [From, Domain]),
     {noreply, State};
@@ -96,65 +39,12 @@ handle_cast({lookup, From, Domain, Type}, State) ->
 handle_cast(_Request, State)                     ->
     {noreply, State}.
 
-async_lookup(From, Domain)       ->
-    gen_server:cast(?SERVER, {lookup, From, Domain}).
-async_lookup(From, Domain, Type) ->
-    gen_server:cast(?SERVER, {lookup, From, Domain, Type}).
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%% @end
-%%--------------------------------------------------------------------
--spec handle_info(Info :: timeout() | term(), State :: term()) ->
-          {noreply, NewState :: term()} |
-          {noreply, NewState :: term(), Timeout :: timeout()} |
-          {noreply, NewState :: term(), hibernate} |
-          {stop, Reason :: normal | term(), NewState :: term()}.
 handle_info(_Info, State) ->
     {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%% @end
-%%--------------------------------------------------------------------
--spec terminate(Reason :: normal | shutdown | {shutdown, term()} | term(),
-                State :: term()) -> any().
 terminate(_Reason, _State) ->
     ok.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%% @end
-%%--------------------------------------------------------------------
--spec code_change(OldVsn :: term() | {down, term()},
-                  State :: term(),
-                  Extra :: term()) -> {ok, NewState :: term()} |
-          {error, Reason :: term()}.
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called for changing the form and appearance
-%% of gen_server status when it is returned from sys:get_status/1,2
-%% or when it appears in termination error logs.
-%% @end
-%%--------------------------------------------------------------------
--spec format_status(Opt :: normal | terminate,
-                    Status :: list()) -> Status :: term().
 format_status(_Opt, Status) ->
     Status.
 
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
