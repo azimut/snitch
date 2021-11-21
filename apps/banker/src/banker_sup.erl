208c9bff3104d829d57e56670a45b666af46b7b7
azimut <azimut.github@protonmail.com>
2021-11-21 13:38:30 -0300

    banker MVP

--- apps/banker/src/banker_sup.erl
+++ apps/banker/src/banker_sup.erl
@@ -29,7 +29,12 @@ init([]) ->
     SupFlags = #{strategy => one_for_all,
                  intensity => 0,
                  period => 1},
-    ChildSpecs = [],
-    {ok, {SupFlags, ChildSpecs}}.
+    ChildSpc = #{id        => banker,
+                 start     => {banker_vault,start_link,[]},
+                 restart   => permanent,
+                 shutdown  => 2000,
+                 type      => worker,
+                 modules   => [banker_vault]},
+    {ok, {SupFlags, [ChildSpc]}}.
 
 %% internal functions
--- apps/banker/src/banker_vault.erl
+++ apps/banker/src/banker_vault.erl
@@ -3,23 +3,40 @@
 -export([start_link/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3, format_status/2]).
+-export([insert/3]).
+
 -define(SERVER, ?MODULE).
--record(state, {}).
+-record(state, {conn}).
+
+insert(Domain, Type, Data) ->
+    gen_server:cast(?SERVER, {insert, Domain, Type, Data}).
 
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 init([]) ->
     process_flag(trap_exit, true),
-    {ok, #state{}}.
+    {ok, C} = epgsql:connect(
+                #{
+                  host => "127.0.0.1",
+                  username => "autoaim",
+                  password => "zaq12wsx",
+                  database => "binance",
+                  timeout => 4000
+                 }),
+    {ok, #state{conn=C}}.
+
+handle_cast({insert, Domain, Type, [H,_]}, #state{conn=C}=State) ->
+    Query = "INSERT INTO dns_what (name,rtype,data) VALUES ($1,$2,$3)",
+    Parameters = [Domain, Type, H],
+    {ok, 1} = epgsql:equery(C, Query, Parameters),
+    {noreply, State};
+handle_cast(_Request, State) ->
+    {noreply, State}.
 
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
-
-handle_cast(_Request, State) ->
-    {noreply, State}.
-
 handle_info(_Info, State) ->
     {noreply, State}.
 terminate(_Reason, _State) ->
