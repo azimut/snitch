2ad863cff72cf2dfd5006f84269403630c66bbd4
azimut <azimut.github@protonmail.com>
2021-11-22 23:50:48 -0300

    revolver & banker working

--- apps/banker/src/banker_vault.erl
+++ apps/banker/src/banker_vault.erl
@@ -3,13 +3,13 @@
 -export([start_link/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3, format_status/2]).
--export([insert/3]).
+-export([insert/5]).
 
 -define(SERVER, ?MODULE).
 -record(state, {conn}).
 
-insert(Domain, Type, Data) ->
-    gen_server:cast(?SERVER, {insert, Domain, Type, Data}).
+insert(Status, Domain, QType, RType, RList) ->
+    gen_server:cast(?SERVER, {insert, Status, Domain, QType, RType, RList}).
 
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
@@ -17,19 +17,24 @@ start_link() ->
 init([]) ->
     process_flag(trap_exit, true),
     {ok, C} = epgsql:connect(
-                #{
-                  host => "127.0.0.1",
+                #{host     => "127.0.0.1",
                   username => "autoaim",
                   password => "zaq12wsx",
                   database => "binance",
-                  timeout => 4000
-                 }),
+                  timeout  => 4000}),
     {ok, #state{conn=C}}.
 
-handle_cast({insert, Domain, Type, [H,_]}, #state{conn=C}=State) ->
-    Query = "INSERT INTO dns_what (name,rtype,data) VALUES ($1,$2,$3)",
-    Parameters = [Domain, Type, H],
-    {ok, 1} = epgsql:equery(C, Query, Parameters),
+handle_cast({insert,_Status,_Domain,_QType,_RType, []}, State) ->
+    {noreply, State};
+handle_cast({insert, Status, Domain, QType, RType, [H|T]}, State)
+  when erlang:is_list(H) ->
+    handle_cast({insert, Status, Domain, QType, RType, H}, State),
+    handle_cast({insert, Status, Domain, QType, RType, T}, State),
+    {noreply, State};
+handle_cast({insert, Status, Domain, QType, RType, Result}, #state{conn=C}=State) ->
+    Query = "INSERT INTO dns_data (domain_name,qtype,rtype,response,rcode) VALUES ($1,$2,$3,$4,$5) ON CONFLICT DO NOTHING",
+    Parameters = [Domain, QType, RType, Result, Status],
+    {ok, _} = epgsql:equery(C, Query, Parameters),
     {noreply, State};
 handle_cast(_Request, State) ->
     {noreply, State}.
--- apps/sheriff/src/sheriff_revolver.erl
+++ apps/sheriff/src/sheriff_revolver.erl
@@ -28,15 +28,17 @@ answers(Error)
 answers_type([])                     -> nil;
 answers_type([#dns_rr{type=Type}|_]) -> Type.
 
-parse([])                      -> [];
-parse([X|Xs])                  -> [parse(X)] ++ parse(Xs);
-parse(X=#dns_rr{type = a})     -> inet:ntoa(X#dns_rr.data);
-parse(X=#dns_rr{type = aaaa})  -> inet:ntoa(X#dns_rr.data);
-parse(X=#dns_rr{type = mx})    -> erlang:element(2, X#dns_rr.data);
-parse(X=#dns_rr{type = soa})   -> erlang:tuple_to_list(X#dns_rr.data);
-parse(X=#dns_rr{type = ns})    -> X#dns_rr.data;
-parse(X=#dns_rr{type = cname}) -> X#dns_rr.data;
-parse(X=#dns_rr{type = txt})   -> lists:nth(1,X#dns_rr.data).
+parse([])                         -> [];
+parse([X=#dns_rr{type = soa}|Xs]) ->  parse(X)  ++ parse(Xs);
+parse([X|Xs])                     -> [parse(X)] ++ parse(Xs);
+parse(X=#dns_rr{type = aaaa})     -> inet:ntoa(X#dns_rr.data);
+parse(X=#dns_rr{type = a})        -> inet:ntoa(X#dns_rr.data);
+parse(X=#dns_rr{type = cname})    -> X#dns_rr.data;
+parse(X=#dns_rr{type = mx})       -> erlang:element(2, X#dns_rr.data);
+parse(X=#dns_rr{type = ns})       -> X#dns_rr.data;
+parse(X=#dns_rr{type = soa})      -> lists:filter(fun erlang:is_list/1,
+                                                  erlang:tuple_to_list(X#dns_rr.data));
+parse(X=#dns_rr{type = txt})      -> lists:nth(1,X#dns_rr.data).
 
 %% Defaults
 
