4bfd3d3c3501f1dcbb7a6404bc447a46b1359c18
azimut <azimut.github@protonmail.com>
2023-01-17 14:44:57 -0300

    sql: unique columns, primary key id, timestamptz

--- create.sql
+++ create.sql
@@ -1,18 +1,22 @@
 create table if not exists dns_data(
-  id serial,
-  created timestamp default now(),
+  id serial not null,
+  created timestamptz not null default now(),
   domain_name varchar(512) not null,
   qtype varchar(16) not null,
   rtype varchar(16) not null,
   response varchar(256),
-  dns inet
+  dns inet,
+  unique(domain_name,qtype,rtype,response),
+  primary key(id)
 );
 
 create table if not exists dns_error(
-  id serial,
-  created timestamp default now(),
+  id serial not null,
+  created timestamptz default now(),
   domain_name varchar(512) not null,
   qtype varchar(16) not null,
   rerror varchar(128) not null,
-  dns inet
+  dns inet,
+  unique(domain_name,qtype,rerror),
+  primary key(id)
 );
