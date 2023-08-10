b4553ecbc9a5068b07f952e73574d231eb0477b0
azimut <azimut.github@protonmail.com>
2023-08-10 14:29:15 -0300

    Format time and date on pinboard

--- apps/pinboard/src/handler_domains.erl
+++ apps/pinboard/src/handler_domains.erl
@@ -5,9 +5,17 @@ init(Req, State) ->
     Headers = #{<<"content-type">> => <<"text/plain">>},
     ClockState = clock:state(),
     Lines = lists:map(fun ({Domain, Timeout}) ->
-                              string:join([Domain, erlang:integer_to_list(Timeout)], " ")
+                              string:join([Domain, human_time(Timeout)], " ")
                       end,
                       ClockState),
     Body = string:join(Lines, "\n"),
     Res = cowboy_req:reply(200, Headers, Body, Req),
     {ok, Res, State}.
+
+human_time(Seconds) ->
+    {H, M, S} = calendar:seconds_to_time(Seconds),
+    human_time(H, M, S).
+
+human_time(0, 0, S) -> io_lib:format("~ps", [S]);
+human_time(0, M, S) -> io_lib:format("~pm ~ps", [M, S]);
+human_time(H, M, S) -> io_lib:format("~ph ~pm ~pm", [H, M, S]).
--- apps/pinboard/src/handler_latest.erl
+++ apps/pinboard/src/handler_latest.erl
@@ -5,8 +5,9 @@
 init(Req, State) ->
     Headers = #{<<"content-type">> => <<"text/plain">>},
     RawRows = banker:latest(?NUMBER_OF_ROWS),
-    Rows = lists:map(fun ({_, Domain, Type, Result}) ->
-                             io_lib:format("~s ~s ~s", [Domain, Type, Result])
+    Rows = lists:map(fun ({{{Y,M,D},_}, Domain, Type, Result}) ->
+                             io_lib:format("~p/~p/~p ~s ~s ~s",
+                                           [Y, M, D, Domain, Type, Result])
                      end,
                      RawRows),
     Body = string:join(Rows, "\n"),
