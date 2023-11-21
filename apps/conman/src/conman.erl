48d3db2cb9cce3150d9082f456538f815be89a85
azimut <azimut.github@protonmail.com>
2023-11-21 11:59:54 -0300

    pinboard: display number of disconnections

--- apps/conman/src/conman.erl
+++ apps/conman/src/conman.erl
@@ -2,7 +2,8 @@
 
 -export([is_connected/0
         ,is_disconnected/0
-        ,check_connectivity/0]).
+        ,check_connectivity/0
+        ,get_disconnections/0]).
 
 -spec is_connected() -> boolean().
 is_connected() -> conman_watchtower:is_connected().
@@ -12,3 +13,6 @@ is_disconnected() -> conman_watchtower:is_disconnected().
 
 -spec check_connectivity() -> ok.
 check_connectivity() -> conman_guard:check_connectivity().
+
+-spec get_disconnections() -> non_neg_integer().
+get_disconnections() -> conman_watchtower:get_disconnections().
--- apps/pinboard/src/handler_root.erl
+++ apps/pinboard/src/handler_root.erl
@@ -3,9 +3,13 @@
 
 init(Req, State) ->
     Headers = #{<<"content-type">> => <<"text/plain">>},
-    Body = io_lib:format("/domains: ~p~n/nameservers: ~p~n/events: ~p~n",
+    Body = io_lib:format("/domains: ~p~n"
+                         ++ "/nameservers: ~p~n"
+                         ++ "/events: ~p~n~n"
+                         ++ "No disconnections: ~p~n",
                          [banker:count_domains()
                          ,banker:count_nameservers()
-                         ,banker:count_events()]),
+                         ,banker:count_events()
+                         ,conman:get_disconnections()]),
     Res = cowboy_req:reply(200, Headers, Body, Req),
     {ok, Res, State}.
